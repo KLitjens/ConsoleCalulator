e9eda268e34c6b3b46b521d307a373d811241999
KLitjens <49155513+KLitjens@users.noreply.github.com>
2019-04-01 19:58:48 +0100

    Add files via upload

---
+++ Calc/Calc/Program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Calculator();
+
+        }
+
+        static void Calculator()
+        {
+            double num1 = 0.0;
+            double num2 = 0.0;
+
+            Console.WriteLine("Please enter the first number:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the following to the desired opperation: \n \tAddition = [+] \n\tSubtraction = [-] \n\tMultiplication = [*] \n\tDivision = [/]");
+            char operation = Convert.ToChar(Console.Read());
+            double result;
+            Console.WriteLine("Result of " + num1 + operation + num2);
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine(result);
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine(result);
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine(result);
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    Console.WriteLine(result);
+                    break;
+            }
+            Console.WriteLine("Press Esc to close, or any other key to start again");
+            Main();
+        }
+
+ 
+    }
+}
